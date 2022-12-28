327136c225b419e3d8d60941b6fa1c6adf3595ed
Valeriy <58651985+alfainter@users.noreply.github.com>
2022-12-28 14:28:05 +0200

    Create Pract1.cpp

---
+++ Pract1.cpp
@@ -0,0 +1,28 @@
+//1
+#include <iostream>
+#include <math.h>
+int main()
+{
+    int a = 2;
+    float b = 11.5;
+    double y;   
+     
+    y = (pow((0.1 * log(pow(M_E,3)))/pow(sin((a/b) * M_PI),2)+pow(cos((a/b) * M_PI),2), 1/3));
+
+    std::cout << y;
+    return(0);
+}
+//2
+#include <iostream>
+#include <math.h>
+int main()
+{
+    int a = 2;
+    float b = 11.5;
+    double y;   
+     
+    y = (pow((0.1 * log(pow(M_E,3)))/pow(sin((a/b) * M_PI),2)+pow(cos((a/b) * M_PI),2), 1/3));
+
+    std::cout << y;
+    return(0);
+}
